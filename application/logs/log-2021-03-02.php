b5bb1b8284bcd0798c6cdd2e246c276a1e8a487a
Khusela Mphokeli <innothetechgeek@gmail.com>
2021-03-02 19:12:36 +0200

    first commit

---
+++ application/logs/log-2021-03-02.php
@@ -0,0 +1,9 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2021-03-02 18:00:45 --> Severity: Notice --> Trying to get property 'contact_type' of non-object C:\wamp64\www\igenerate\application\core\ClientsController.php 67
+ERROR - 2021-03-02 18:02:58 --> Severity: Notice --> Undefined index: admin-auth C:\wamp64\www\igenerate\application\libraries\assets\App_css.php 46
+ERROR - 2021-03-02 18:02:58 --> Severity: Warning --> array_keys() expects parameter 1 to be array, null given C:\wamp64\www\igenerate\application\libraries\assets\App_css.php 46
+ERROR - 2021-03-02 18:04:28 --> Severity: Notice --> Undefined variable: email C:\wamp64\www\igenerate\application\controllers\Authentication.php 486
+ERROR - 2021-03-02 18:04:29 --> Severity: Notice --> Undefined index: nok_cell_number C:\wamp64\www\igenerate\application\views\email\agentsignup.php 88
+ERROR - 2021-03-02 18:04:32 --> Severity: Notice --> Undefined index: admin-auth C:\wamp64\www\igenerate\application\libraries\assets\App_css.php 46
+ERROR - 2021-03-02 18:04:32 --> Severity: Warning --> array_keys() expects parameter 1 to be array, null given C:\wamp64\www\igenerate\application\libraries\assets\App_css.php 46
