07a3db6dad093eca20ded1b787b1d5d9443414b6
Khusela <innothetechgeek@gmail.com>
2021-03-03 10:04:10 +0200

    contact type migration

--- application/migrations/279_version_279.php
+++ application/migrations/279_version_279.php
@@ -5,10 +5,10 @@ class Migration_Version_279 extends CI_Migration
     public function up()
     {
         
-            $fields = array(
-                  'contact_type' => array('type' => 'TEXT')
-            );
-            $this->dbforge->add_column('contacts', $fields);
+            // $fields = array(
+            //       'contact_type' => array('type' => 'TEXT')
+            // );
+            // $this->dbforge->add_column('contacts', $fields);
 
    }
 
